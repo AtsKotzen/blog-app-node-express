cdfd11cddd94c9e9e5fafe3ab1f0d11b60bb7d62
AtsKotzen <atsaudio@gmail.com>
2022-04-16 03:15:11 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const {engine} = require('express-handlebars');
+const bodyParser = require('body-parser');
+//const mongoose = require('mongoose');
+const app = express();
+const adminRoutes = require('./routes/admin');
+const path = require('path');
+
+// Configure the app
+    // Handlebars
+    app.engine('handlebars', engine({ extname: '.hbs', defaultLayout: "main"}));    app.set('view engine', 'handlebars');
+    app.set('view engine', 'handlebars');
+    app.set("views", "./views");
+    // Body Parser
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+
+    // Mongoose
+      // Soon
+
+    // Public
+    app.use(express.static(path.join(__dirname, 'public')));    // Routes
+    app.use('/admin', adminRoutes);
+
+// Others
+const PORT = 8081
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
---
+++ routes/admin.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.render('admin/index');
+})
+router.get('/posts', (req, res) => {
+    res.send('Posts page');
+})
+router.get('/categories', (req, res) => {
+    res.send('Categories page');
+})
+
+module.exports = router;
